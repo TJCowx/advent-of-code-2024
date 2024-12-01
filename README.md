58a0e77e34891f3c8f5c8135fe37fb4aa816abb5
TJCowx21 <tcowx21@gmail.com>
2024-12-01 14:04:50 -0500

    Day 1

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Advent of Code 2024
+
+Just learning go by AoC 2024
+
+## How to Run
+```bash
+# go run main.go day-part
+go run main.go 1-2 # Runs day 1 part 2
+go run main.go 1 # Runs just day 1
+```
+
+## Days
+
+1. 🌟🌟
+2.
+3.
+4.
+5.
+6.
+7.
+8.
+9.
+10.
+11.
+12.
+13.
+14.
+15.
+16.
+17.
+18.
+19.
+20.
+21.
+22.
+23.
+24.
+25.
--- day01/day01.go
+++ day01/day01.go
@@ -1,8 +1,14 @@
 package day01
 
+// https://adventofcode.com/2024/day/1
+
 import (
 	"advent-of-code-2024/file_reader"
 	"fmt"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func Run(part *string) {
@@ -18,17 +24,74 @@ func Run(part *string) {
 	}
 }
 
-func parse_input() {
-	content := file_reader.Read("day01/test-input.txt")
+func parse_lists() ([]int, []int) {
+	content := file_reader.Read("day01/input.txt")
 
-	fmt.Printf(content)
+	lines := strings.Split(content, "\n")
+
+	// Get rid of that empty line
+	if len(lines) > 0 && lines[len(lines)-1] == "" {
+		lines = lines[:len(lines)-1]
+	}
+
+	var listA []int
+	var listB []int
+
+	for _, line := range lines {
+		inputs := strings.Fields(line)
+		a, b := inputs[0], inputs[1]
+		numA, _ := strconv.Atoi(a)
+		numB, _ := strconv.Atoi(b)
+		listA = append(listA, numA)
+		listB = append(listB, numB)
+	}
+
+	return listA, listB
 }
 
 func part1() {
 	fmt.Println("Day 01, Part 1")
-	parse_input()
+
+	listA, listB := parse_lists()
+
+	sort.Ints(listA)
+	sort.Ints(listB)
+
+	var sum int = 0
+
+	for i := 0; i < len(listA); i++ {
+		diff := listA[i] - listB[i]
+
+		sum += int(math.Abs(float64(diff)))
+	}
+
+	fmt.Printf("ANSWER IS: %d\n", sum)
 }
 
 func part2() {
 	fmt.Println("Day 01, Part 2")
+
+	listA, listB := parse_lists()
+
+	countMap := map[int]int{}
+
+	for _, item := range listB {
+		val, exists := countMap[item]
+		if exists {
+			countMap[item] = val + 1
+		} else {
+			countMap[item] = 1
+		}
+	}
+
+	var sum int = 0
+
+	for _, item := range listA {
+		count, exists := countMap[item]
+		if exists {
+			sum += (item * count)
+		}
+	}
+
+	fmt.Printf("ANSWER IS %d\n", sum)
 }
