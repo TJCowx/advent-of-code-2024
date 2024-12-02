a9496f42fa82a638a88be685170d905d4c7977ac
TJCowx21 <tcowx21@gmail.com>
2024-12-02 18:43:44 -0500

    day2 again

---
+++ day02/day02_test.go
@@ -0,0 +1,23 @@
+package day02
+
+import "testing"
+
+var TEST_PATH = "./test-input.txt"
+
+//func TestPart1(t *testing.T) {
+//	got := part1(TEST_PATH)
+//	expected := 2
+
+//	if got != expected {
+//		t.Errorf("Got %d, expected %d", got, expected)
+//	}
+//}
+
+func TestPart2(t *testing.T) {
+	got := part2(TEST_PATH)
+	expected := 14
+
+	if got != expected {
+		t.Errorf("Got %d, expected %d", got, expected)
+	}
+}
