b5483daa5542eddab9a63327aa640f93ba74bd0c
TJCowx21 <tcowx21@gmail.com>
2024-12-05 00:30:21 -0500

    day05 skaffold

--- day05/day05.go
+++ day05/day05.go
@@ -2,23 +2,35 @@ package day05
 
 import "fmt"
 
+var INPUT_PATH = "day05/input.txt"
+
 func Run(part *string) {
-    if part == nil {
-        part1()
-        part2()
-    } else if *part == "1" {
-        part1()
-    } else if *part == "2" {
-        part2()
-    } else {
-        fmt.Println("INVALID INPUT")
-    }
+	if part == nil {
+		part1(INPUT_PATH)
+		part2(INPUT_PATH)
+	} else if *part == "1" {
+		part1(INPUT_PATH)
+	} else if *part == "2" {
+		part2(INPUT_PATH)
+	} else {
+		fmt.Println("INVALID INPUT")
+	}
 }
 
-func part1() {
-    fmt.Println("NOT IMPLEMENTED")
+func part1(path string) int {
+	fmt.Println("DAY 05 PART 1")
+	fmt.Println("NOT IMPLEMENTED")
+
+	// Map out rules, each number has a set of rules that must be adhered, only if both are in it
+	// Data structure???????
+	// Map out order
+
+	return 0
 }
 
-func part2() {
-    fmt.Println("NOT IMPLEMENTED")
+func part2(path string) int {
+	fmt.Println("DAY 05 PART 2")
+	fmt.Println("NOT IMPLEMENTED")
+
+	return 0
 }
